9991649199f814a310078adff9597334be191c42
NiceGoodtimes <william.s.elwell1@gmail.com>
2025-09-24 10:33:33 +0200

    Renamed variables for readability

--- From_Physics_Class/triangular_vol_uncertainty.py
+++ From_Physics_Class/triangular_vol_uncertainty.py
@@ -1,25 +1,25 @@
 """VARIABLES"""
 #input individual averages
-mean_a = float(input("enter the average of b: ")) #takes the average of several measurements of a base
-mean_b = float(input("enter the average of h: ")) #takes the average of several measurements of a height
-mean_c = float(input("enter the average of t: ")) #takes the average of several measurements of a thickness
+mean_base = float(input("enter the average of b: ")) #takes the average of several measurements of a base
+mean_height = float(input("enter the average of h: ")) #takes the average of several measurements of a height
+mean_thickness = float(input("enter the average of t: ")) #takes the average of several measurements of a thickness
 
 #first product in function
-mean_bc = (mean_b * mean_c)/2
-uncert_a = float(input("enter the uncertainty of a: ")) #takes the uncertainty of the length measurements
+mean_bc = (mean_height * mean_thickness)/2
+uncert_base = float(input("enter the uncertainty of a: ")) #takes the uncertainty of the length measurements
 
 #second product in function
-mean_ac = (mean_a * mean_c)/2
-uncert_b = float(input("enter the uncertainty of b: ")) #takes the uncertainty of the width measurements
+mean_ac = (mean_base * mean_thickness)/2
+uncert_height = float(input("enter the uncertainty of b: ")) #takes the uncertainty of the width measurements
 
 #third product in function
-mean_ab = (mean_a * mean_b)/2
-uncert_c = float(input("enter the uncertainty of c: ")) #takes the uncertainty of the height measurements
+mean_ab = (mean_base * mean_thickness)/2
+uncert_thickness = float(input("enter the uncertainty of c: ")) #takes the uncertainty of the height measurements
 
 """FUNCTIONS"""
 #calculates the uncertainty in the volume of a rectangular prism
 def calculate():
-    inside = (mean_bc*uncert_a)**2 +(mean_ac*uncert_b)**2 +(mean_ab*uncert_c)**2
+    inside = (mean_bc*uncert_base)**2 +(mean_ac*uncert_height)**2 +(mean_ab*uncert_thickness)**2
     uncert_volume = inside**0.5
     return uncert_volume
 
