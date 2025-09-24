ba4b9d63ab3e96151f26cad40908f9ea3135c724
NiceGoodtimes <william.s.elwell1@gmail.com>
2025-09-24 10:52:47 +0200

    updated input prompts

--- From_Physics_Class/triangular_vol_uncertainty.py
+++ From_Physics_Class/triangular_vol_uncertainty.py
@@ -1,20 +1,20 @@
 """VARIABLES"""
 #input individual averages
-mean_base = float(input("enter the average of b: ")) #takes the average of several measurements of a base
-mean_height = float(input("enter the average of h: ")) #takes the average of several measurements of a height
-mean_thickness = float(input("enter the average of t: ")) #takes the average of several measurements of a thickness
+mean_base = float(input("enter the average of the base: ")) #takes the average of several measurements of a base
+mean_height = float(input("enter the average of the height: ")) #takes the average of several measurements of a height
+mean_thickness = float(input("enter the average of the thickness: ")) #takes the average of several measurements of a thickness
 
 #first product in function
 mean_bc = (mean_height * mean_thickness)/2
-uncert_base = float(input("enter the uncertainty of a: ")) #takes the uncertainty of the length measurements
+uncert_base = float(input("enter the uncertainty of the base: ")) #takes the uncertainty of the length measurements
 
 #second product in function
 mean_ac = (mean_base * mean_thickness)/2
-uncert_height = float(input("enter the uncertainty of b: ")) #takes the uncertainty of the width measurements
+uncert_height = float(input("enter the uncertainty of the height: ")) #takes the uncertainty of the width measurements
 
 #third product in function
 mean_ab = (mean_base * mean_thickness)/2
-uncert_thickness = float(input("enter the uncertainty of c: ")) #takes the uncertainty of the height measurements
+uncert_thickness = float(input("enter the uncertainty of the thickness: ")) #takes the uncertainty of the height measurements
 
 """FUNCTIONS"""
 #calculates the uncertainty in the volume of a rectangular prism
